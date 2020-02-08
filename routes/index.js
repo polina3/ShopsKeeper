fb14d0b9589698c3c3eb67c9a7784c22294602c1
Полина Журавлёва <36899449+polina3@users.noreply.github.com>
2020-02-09 02:20:57 +0300

    авторизация
    ,,,,

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,28 @@ const session = require('express-session');
 const fs = require('fs');
 const conf=JSON.parse(fs.readFileSync('config.json'));
 
+var app=express();
+
+app.use(session({
+    key: 'application.sid',
+    secret: 'some.secret.string',
+    cookie: {
+        maxAge: 60 * 60 * 1000,
+        expires: 60 * 60 * 1000
+    },
+    saveUninitialized: true,
+    rolling: true,
+    resave: true,
+    secure: true
+}));
+
 const connection = mysql.createConnection({
 	host: conf.connectionBD.host,
 	user: conf.connectionBD.user,
 	database: conf.connectionBD.database,
 	password: conf.connectionBD.password
   });
+
 connection.connect(function(err){
     if (err) {
       return console.error("Ошибка: " + err.message);
@@ -26,7 +42,20 @@ router.get('/', function (req, res) {
     res.render("index",{});
 });
 router.post('/', function (req, res) {
-    res.render("index",{});
+    rez=[req.body.login,req.body.password];
+  	connection.query(conf.qBD.q,rez,
+    function(err, results) {
+      if(err){
+        console.log(err);
+        return res.render('index',{data:"error"});
+      }
+      console.log(results); 
+      if(results.length==0){
+        return res.render('index',{data:"Непрвильный логин или пароль"});
+      }
+      req.session.login=results[0].login+results[0].buy;
+         
+    });
 });
 
 module.exports = router;
