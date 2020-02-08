b04657e6bdd1a5d589e3e2a475ce1c43f3cdb24a
Полина Журавлёва <36899449+polina3@users.noreply.github.com>
2020-02-09 02:13:27 +0300

    подключение к БД и поправка стилей
    ,,,

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,32 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const session = require('express-session');
+const fs = require('fs');
+const conf=JSON.parse(fs.readFileSync('config.json'));
 
-/* GET home page. */
+const connection = mysql.createConnection({
+	host: conf.connectionBD.host,
+	user: conf.connectionBD.user,
+	database: conf.connectionBD.database,
+	password: conf.connectionBD.password
+  });
+connection.connect(function(err){
+    if (err) {
+      return console.error("Ошибка: " + err.message);
+    }
+    else{
+      console.log("Подключение к серверу MySQL успешно установлено");
+    }
+ });
+
 router.get('/', function (req, res) {
     res.render("index",{});
 });
-
+router.post('/', function (req, res) {
+    res.render("index",{});
+});
 
 module.exports = router;
