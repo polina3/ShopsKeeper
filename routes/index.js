935c4fe2e426261af2190fad7f06e8db2bd88806
Полина Журавлёва <36899449+polina3@users.noreply.github.com>
2020-02-11 23:48:49 +0300

    reg

--- routes/index.js
+++ routes/index.js
@@ -12,21 +12,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 var rez=[];
 
-const connection = mysql.createConnection({
-	host: conf.connectionBD.host,
-	user: conf.connectionBD.user,
-	database: conf.connectionBD.database,
-	password: conf.connectionBD.password
-  });
 
-connection.connect(function(err){
-    if (err) {
-      return console.error("Ошибка: " + err.message);
-    }
-    else{
-      console.log("Подключение к серверу MySQL успешно установлено");
-    }
- });
 
 router.get('/', function (req, res) {
     res.render("index_email",{});
