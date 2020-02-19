22cac42a2ef88dd33896df74df014fd06890ed6c
Полина Журавлёва <36899449+polina3@users.noreply.github.com>
2020-02-20 01:55:04 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,11 @@ const bodyParser = require("body-parser");
 const session = require('express-session');
 const fs = require('fs');
 const conf=JSON.parse(fs.readFileSync('config.json'));
+
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+var pass="";
 
 const connection = mysql.createConnection({
   host: conf.connectionBD.host,
@@ -26,30 +29,9 @@ connection.connect(function(err){
     }
  });
 
-app.use(session({
-    secret: 'secretWord',   // секретное слово для шифрования
-    key: 'key',             // имя куки
-    cookie: {
-        path: '/',          // где действует
-        httpOnly: true,     // чтобы куку не могли читать на клиенте
-        maxAge: null        // время жизни куки
-    },
-    saveUninitialized: false,   // on default
-    resave: false               // on  default
-}));
-
-let checkSignIn = (req, res, next) => {
-    if (req.session.e) {   
-        next();
-    } else {
-        res.redirect("/");
-    }
-}
-
 router.get('/', function (req, res) {
     res.render("index_email",{});
 });
-
 router.post('/', function (req, res) {
   if(req.body.email){
     connection.query(conf.qBD.q1,req.body.email,
@@ -62,36 +44,29 @@ router.post('/', function (req, res) {
         console.log("-");
         return res.render('index_email',{data:"Такого пользователя нет"});
       }
-      const users = results;
-      req.session.e=users[0].email;
       res.render("index_password",{});
-      console.log(req.session.email);
+      console.log("log");
     });
   }
   else if(req.body.password){
-     connection.query(conf.qBD.q2,req.body.password,
-     	function(err, results) {
-        	if(err){
-          		console.log(err);
-          		return res.send("error");
-      		}
-      		if(results.length==0){
-        		console.log("-");
-       			 return res.render('index_password',{data:"Неправильнеый пароль "});
-          }
-          else{
-            res.send("ok");
-          }
-      });
-     connection.end();
-   };
+    let rez=[req.body.email,req.body.password];
+   connection.query(conf.qBD.q2,rez,
+      function(err, results) {
+        if(err){
+          console.log(err);
+          return res.render('index_password',{data:"error"});
+      }
+      if(results.length==0){
+        console.log("-");
+        return res.render('index_password',{data:"неверный пароль"});
+      }
+      res.send("Ok");
+    });
+   connection.end();
+  }
+});
 
 router.get('/reg', function (req, res) {
     res.render("reg",{});
-})
-
-
-
-
-
+});
 module.exports = router;
