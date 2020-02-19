8e40bbd2d31ddb81c5bb210ecffd31e801d84cf6
Полина Журавлёва <36899449+polina3@users.noreply.github.com>
2020-02-20 01:34:12 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -40,7 +40,7 @@ app.use(session({
 }));
 
 let checkSignIn = (req, res, next) => {
-    if (req.session.email) {   
+    if (req.session.e) {   
         next();
     } else {
         res.redirect("/");
@@ -63,7 +63,7 @@ router.post('/', function (req, res) {
         console.log("-");
         return res.render('index_email',{data:"Такого пользователя нет"});
       }
-      req.session.email=results[0].email;
+      req.session.e=results[0].email;
       res.render("index_password",{});
       console.log(req.session.email);
     });
