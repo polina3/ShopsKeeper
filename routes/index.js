c549a57ace3581ce5dddb208642c7d67a26442a0
Полина Журавлёва <36899449+polina3@users.noreply.github.com>
2020-02-21 02:29:58 +0300

    v4

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,48 @@
-//---------------------------
+//-------настройка все что связанно с expess--------------------
 const express = require('express');
+const session = require('express-session');
+var SessionStore = require('express-mysql-session');
 var router = express.Router();
 var app=express();
-//---------------------------
+//-------SQL--------------------
 const mysql = require("mysql2");
-//---------------------------
+//-------bodyParser--------------------
 const bodyParser = require("body-parser");
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
-//---------------------------
+//-------config.json--------------------
 const fs = require('fs');
 const conf=JSON.parse(fs.readFileSync('./config.json'));
-//---------------------------
+//-------настройка session--------------------
+app.use(session({
+    key: 'session_cookie_name',
+    secret: 'session_cookie_secret',
+    store: new SessionStore(
+    host: conf.connectionBD.host,
+    user: conf.connectionBD.user,
+    database: conf.connectionBD.database,
+    password: conf.connectionBD.password)
+}))
 
+//---------------------------
+var P_END=(pool)=>{
+  pool.end((err)=>{
+    if (err) {
+      console.log(err.message);
+    }
+    console.log("пул закрыт");
+  });
+}
+//---------------------------
+var isEmail=(reg,res, next)=>{
+  if(req.session.email='' || req.session.email){
+    res.redirect('/');
+  }
+  else{
+    return next();
+  }
+}
+//---------------------------
 const pool = mysql.createPool({
     connectionLimit: 500,
     queueLimit:300,
@@ -23,20 +53,7 @@ const pool = mysql.createPool({
     database: conf.connectionBD.database,
     password: conf.connectionBD.password
 });
-
-/*pool.getConnection((connection,err)=>{
-  if(err){
-       console.log(err);
-      connection.release();
-  }
-  else{
-     console.log("OK_SQL");
-      connection.release();
-  }
-     
-    })
-*/
-
+//---------------------------
 router.get('/', function (req, res) {
     res.render("index_email",{});
 });
@@ -52,8 +69,10 @@ router.post('/', function (req, res) {
         return res.render('index_email',{data:"Такого пользователя нет"});
       }
       console.log(results[0].email);
+      req.session.email=results[0].email;
       res.redirect('/password');
-  })          
+  })   
+  P_END(pool);       
 });
 
 
@@ -61,9 +80,21 @@ router.get('/password', function (req, res) {
     res.render("index_password",{});
 });
 
-/*router.post('/password', function (req, res) {
-    res.render("index_password",{});
+router.post('/password',isEmail, function (req, res) {
+  let data=[req.session.email,req.body.password];
+   pool.execute(conf.qBD.q2,data,(err, results)=>{
+    if(err){
+          console.log(err);
+         return res.render('index_password',{data:"error"});
+      }
+    if(results.length==0){
+        console.log("-p");
+        return res.render('index_password',{data:"неверный пароль"});
+      }
+    res.send("Ok");
+  })   
+  P_END(pool);           
 });
-*/
+//---------------------------
 
 module.exports = router;
--- ser.js
+++ ser.js
@@ -1,5 +1,10 @@
+//-------настройка все что связанно с expess--------------------
 const express = require('express');
+const session = require('express-session');
+var SessionStore = require('express-mysql-session');
+var router = express.Router();
 var app=express();
+//-------подключение роутеров--------------------
 var main = require('./routes/index');
 //var api = require('./routes/api');
 const fs = require('fs');
@@ -9,7 +14,16 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 //---------------------------
-
+//-------настройка session--------------------
+app.use(session({
+    key: 'session_cookie_name',
+    secret: 'session_cookie_secret',
+    store: new SessionStore(
+    host: conf.connectionBD.host,
+    user: conf.connectionBD.user,
+    database: conf.connectionBD.database,
+    password: conf.connectionBD.password)
+}))
 
 
 
