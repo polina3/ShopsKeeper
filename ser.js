56c8d43e3b7041f85dc4e66590d8700877c4f6ea
Полина Журавлёва <36899449+polina3@users.noreply.github.com>
2020-02-21 00:57:11 +0300

    Update ser.js

--- ser.js
+++ ser.js
@@ -1,24 +1,19 @@
 const express = require('express');
-const mysql = require("mysql2");
-var path = require('path');
-const session = require('express-session');
-const bodyParser = require("body-parser");
-//var main = require('./routes/index');
-//var api = require('./routes/api');
+var main = require('./routes/index');
+var api = require('./routes/api');
 const conf=require('./config');
 
 var app=express();
 
-/*app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use("/static",express.static('static'));
+
 app.set("view engine", "pug");
 app.use('/', main);
 app.use('/api', api);
 
- console.log("server run");*/
+console.log("server run");
 
 
 
