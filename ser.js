edc8a6146c9c857da0ced11481df17e4ec5da44e
Полина Журавлёва <36899449+polina3@users.noreply.github.com>
2020-02-21 11:05:35 +0300

    Update ser.js

--- ser.js
+++ ser.js
@@ -1,12 +1,11 @@
 //-------настройка все что связанно с expess--------------------
 const express = require('express');
-const session = require('express-session');
-var SessionStore = require('express-mysql-session');
 var router = express.Router();
 var app=express();
 //-------подключение роутеров--------------------
 var main = require('./routes/index');
 var api = require('./routes/api');
+//---------------------------
 const fs = require('fs');
 const conf=JSON.parse(fs.readFileSync('config.json'));
 //---------------------------
@@ -14,20 +13,6 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 //---------------------------
-var options = {
- 	host: conf.connectionBD.host,
-    user: conf.connectionBD.user,
-    database: conf.connectionBD.database,
-    password: conf.connectionBD.password
-}
-
-//-------настройка session--------------------
-app.use(session({
-    key: 'session_cookie_name',
-    secret: 'session_cookie_secret',
-    store: new SessionStore(options)
-}))
-
 
 
 app.use("/static",express.static('static'));
