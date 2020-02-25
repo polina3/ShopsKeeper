530d644e922d7fafca35feaf5347760b40eafb61
Полина Журавлёва <36899449+polina3@users.noreply.github.com>
2020-02-25 23:01:22 +0300

    reg

--- routes/reg.js
+++ routes/reg.js
@@ -94,7 +94,7 @@ router.post('/', function (req, res) {
             req.body.date,
             req.body.gender
           ];
-        pool.execute(conf.qBD.S_U,a)
+        pool.execute(conf.qBD.C_U,a)
         .then(() =>{
           res.send('OK');
        })
