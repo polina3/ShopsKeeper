b75685b7234b9632dcd80dd4e03d7154beacd93c
Полина Журавлёва <36899449+polina3@users.noreply.github.com>
2020-03-04 22:52:36 +0300

    pp

--- routes/PersonalPage.js
+++ routes/PersonalPage.js
@@ -42,8 +42,7 @@ router.get('/', function (req, res) {
   console.log(req.session.email);
   pool.execute(conf.qBD.Shop,req.session.email)
   .then((result)=>{
-    console.log(result);
-     res.render("PersonalPage",{login:req.session.email,shops:[4,5]});
+    res.render("PersonalPage",{login:req.session.email);
   })
 });
 
