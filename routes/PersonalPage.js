5145655256032d3278d7efbb30afd7467a8d1477
Полина Журавлёва <36899449+polina3@users.noreply.github.com>
2020-03-06 00:46:30 +0300

    Update PersonalPage.js

--- routes/PersonalPage.js
+++ routes/PersonalPage.js
@@ -35,7 +35,7 @@ router.get('/', function (req, res) {
   console.log(req.session.email);
   pool.execute(conf.qBD.Shop,[req.session.email])
   .then((result)=>{
-    console.log(result);
+    fs.writeFileSync("hello.txt", result);
     res.render("PersonalPage",{login:req.session.email,shops:[4,5]});
   })
 });
