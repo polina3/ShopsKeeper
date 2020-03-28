11b015e20662cc8ff2a7b128c127d0c257863e49
Полина Журавлёва <36899449+polina3@users.noreply.github.com>
2020-03-28 18:50:31 +0300

    Create AddProduct.js

---
+++ routes/AddProduct.js
@@ -0,0 +1,29 @@
+//-------настройка все что связанно с expess--------------------
+const express = require('express');
+var router = express.Router();
+var app=express();
+//-------SQL--------------------
+const mysql = require("mysql2");
+//-------config.json--------------------
+const fs = require('fs');
+const conf=JSON.parse(fs.readFileSync('config.json'));
+
+
+//---------------------------
+const pool = mysql.createPool({
+    connectionLimit: 500,
+    queueLimit:300,
+    waitForConnections:true,
+
+    host: conf.connectionBD.host,
+    user: conf.connectionBD.user,
+    database: conf.connectionBD.database,
+    password: conf.connectionBD.password
+}).promise();
+//---------------------------
+
+router.get('/', function (req, res) {
+  res.render("AddShop",{});
+});
+
+module.exports = router;
