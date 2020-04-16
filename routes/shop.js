e535d9b2daa3176c5ae2713c8a6ce853e997e0d9
Полина Журавлёва <36899449+polina3@users.noreply.github.com>
2020-04-16 22:58:51 +0300

    Update shop.js

--- routes/shop.js
+++ routes/shop.js
@@ -27,7 +27,7 @@ router.get('/:s', function (req, res) {
   	pool.execute(conf.qBD.Product,[req.params["s"]])
   .then((result)=>{
   	console.log(result[0]);
-    res.render("shop",{name:req.params["s"],product:result[0],i:req.params["s"]})
+    res.render("shop",{name:req.params["s"],product:result[0]})
 })
 })
 
